303dac32d29cf766e38f954dcd266f8fff9ec947
fengmk2 <fengmk2@gmail.com>
2013-09-06 01:51:15 +0800

    add stat() and writeFile()

--- test/qnfs.test.js
+++ test/qnfs.test.js
@@ -1,6 +1,6 @@
 /*!
  * qnfs - test/qnfs.test.js
- * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com>
+ * Copyright(c) 2013 fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
  * MIT Licensed
  */
 
@@ -10,9 +10,38 @@
  * Module dependencies.
  */
 
-var qnfs = require('../');
+var path = require('path');
+var fs = require('fs');
 var should = require('should');
+var qnfs = require('../');
+
+var fixtures = path.join(__dirname, 'fixtures');
 
 describe('qnfs.test.js', function () {
-  
+  before(function (done) {
+    qnfs.config(require('./config.json'));
+    qnfs.writeFile('/qnfs/test/fixtures/foo.txt', fs.readFileSync(path.join(fixtures, 'foo.txt')), done);
+  });
+
+  describe('stat()', function () {
+    it('should get a file stat', function (done) {
+      qnfs.stat('/qnfs/test/fixtures/foo.txt', function (err, stat) {
+        should.not.exist(err);
+        should.exist(stat);
+        stat.should.have.keys('size', 'atime', 'mtime', 'ctime');
+        stat.size.should.equal(fs.statSync(path.join(fixtures, 'foo.txt')).size);
+        done();
+      });
+    });
+
+    it('should return error when file not exists', function (done) {
+      qnfs.stat('/qnfs/test/fixtures/foo.not.exists.txt', function (err, stat) {
+        should.exist(err);
+        err.name.should.equal('QiniuFileNotExistsError');
+        err.message.should.equal('no such file or directory');
+        should.not.exist(stat);
+        done();
+      });
+    });
+  });
 });
